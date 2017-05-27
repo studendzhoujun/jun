1a0cecb1a595916d76dd1160a0cb5ed2f67c832a
zhoujun <zhoujun@gomeplus.com>
2017-05-27 15:59:32 +0800

    addlogger

--- server/foods.js
+++ server/foods.js
@@ -2,16 +2,16 @@
  * @Author: zhouJun 
  * @Date: 2017-05-26 14:02:48 
  * @Last Modified by: zhouJun
- * @Last Modified time: 2017-05-26 14:06:41
+ * @Last Modified time: 2017-05-27 15:58:32
  */
 const express =require('express');
 const router =express.Router();
-
+const logger = require('morgan');
 router.use(function(req,res,next){
      console.log('time:',Date.now());
      next();
 })
-
+router.use(logger('dev'));
 router.get('/',function(req,res){
     res.send('foods home page');
 });
